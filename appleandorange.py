455963fa231adfc76c6ebffdd897decb658902e3
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-10-23 20:38:18 +0800

    Attaching Integer Function into a Parameter and Setting Up an Operation into a Variable

--- appleandorange.py
+++ appleandorange.py
@@ -3,3 +3,8 @@ price_apple = 20
 price_orange = 25
 apple = input("Please quantify how many apples you need to purchase: ")
 orange = input("Please quantify how many oranges you need to purchase: ")
+
+apple_string = int(apple)
+orange_string = int(orange)
+apple_total = (apple * price_apple)
+orange_total = (orange * price_orange)
