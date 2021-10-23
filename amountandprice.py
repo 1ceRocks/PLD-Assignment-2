46c4f45208a2c7f07bc71e1ea5592bf752e41db0
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-10-23 21:25:47 +0800

    Adding Python Arithmetic Operators

--- amountandprice.py
+++ amountandprice.py
@@ -1,3 +1,5 @@
 money = float(input("How much money do you have? "))
 apple = float(input("What is the price of an apple per piece? "))
 
+exchange = money % apple
+apple_maxQuantity = money // apple
