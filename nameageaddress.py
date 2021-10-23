a729d1bc69e85b3eb60c7dce5853d5aac6f02c75
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-10-23 21:50:58 +0800

    Code Completed

--- nameageaddress.py
+++ nameageaddress.py
@@ -2,4 +2,4 @@ name = input("Enter your name: ")
 age = input("Enter your age: ")
 address = input("Enter your address: ")
 
-print(f"Hi, my name is {name}. I am {age} years old and I live in {address}.")
+print(f"Hi, my name is {name}. I am {age} years old and I live in {address} .")
