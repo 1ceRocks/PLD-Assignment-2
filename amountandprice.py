a164f13b4b553a25cd0ff670fff8ad72c66828d9
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-10-23 21:45:19 +0800

    Code Completed

--- amountandprice.py
+++ amountandprice.py
@@ -4,4 +4,4 @@ apple = float(input("What is the cost of an apple per item? "))
 exchange = money % apple
 apple_maxQuantity = money // apple
 
-print(f"You can buy {apple_maxQuantity: .2f} apples and your change is {exchange: .2f}.")
+print(f"You can buy {apple_maxQuantity} apples and your change is {exchange: .2f}.")
