94cf930e8475d6a8a72c588f8df7dfb09ef7c171
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-10-23 20:32:50 +0800

    Setting an Input in my Parameters and Quantifying them in Pricing

---
+++ appleandorange.py
@@ -0,0 +1,5 @@
+print("The Price of Apple is 20, while the Price of Orange is 25")
+price_apple = 20
+price_orange = 25
+apple = input("Please quantify how many apples you need to purchase: ")
+orange = input("Please quantify how many oranges you need to purchase: ")
