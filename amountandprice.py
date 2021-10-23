5779fb52db850fda0e45b0a018d81280ff2df55c
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-10-23 21:42:32 +0800

    Code Completed

--- amountandprice.py
+++ amountandprice.py
@@ -1,5 +1,7 @@
-money = float(input("How much money do you have? "))
-apple = float(input("What is the price of an apple per piece? "))
+money = float(input("How much cash do you possess right now? "))
+apple = float(input("What is the cost of an apple per item? "))
 
 exchange = money % apple
 apple_maxQuantity = money // apple
+
+print(f"You can buy {apple_maxQuantity: .2f} apples and your change is {exchange: .2f}.")
