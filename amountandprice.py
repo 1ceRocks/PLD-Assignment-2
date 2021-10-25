665ad97a1024a539c058ea77e50217622ee9fcc9
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-10-26 00:13:16 +0800

    CODE COMPLETED: ADDITIONAL FEATURE

--- amountandprice.py
+++ amountandprice.py
@@ -1,7 +1,13 @@
-money = float(input("How much cash do you possess right now? "))
-apple = float(input("What is the cost of an apple per item? "))
+#Rendered a welcoming greet to a user after the initialization of the program.
+print("\nWelcome to Villariza Foods! Our available product for today is apple. \n")
+money = float(input("How much cash do you possess right now? \n> PHP: "))
+apple = float(input("\nWhat is the cost of an apple per item? \n> PHP: "))
 
-exchange = money % apple
-apple_maxQuantity = money // apple
-
-print(f"You can buy {apple_maxQuantity} apples and your change is {exchange: .2f}.")
+#Displayed an aditional feature and response when a user does not have enough money to buy a single quantity of Apple.
+if money >= apple:
+    exchange = money % apple
+    apple_maxQuantity = int(money // apple)
+    print(f"\nYou can buy {apple_maxQuantity} apples and your change is {exchange: .2f} PHP.")
+else:
+    moneyShortage = float(apple - money)
+    print(f"\nSorry, but you do not have enough money to buy an apple. You need {moneyShortage: .2f} PHP in order to purchase a single apple.")
