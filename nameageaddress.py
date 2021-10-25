d4c0b6be1d6f0a9c572942f1cdd377daec1e89e7
Fritz Cedrick Villariza <villarizaced@gmail.com>
2021-10-26 01:12:57 +0800

    CODE COMPLETED: ADDITIONAL FEATURE

--- nameageaddress.py
+++ nameageaddress.py
@@ -1,5 +1,9 @@
-name = input("Enter your name: ")
-age = input("Enter your age: ")
-address = input("Enter your address: ")
+#Arranged the variables and the spacing between the input function for easy viewing.
+print("\nYour Personal Information")
+userName = input("\nEnter your NAME here. \n> ")
+userAge = int(input("\nEnter your AGE here in numbers. \n> "))
+userAddress = input("\nEnter your ADDRESS here. \n> ")
 
-print(f"Hi, my name is {name}. I am {age} years old and I live in {address} .")
+#Annexed an additional print response acting as a comment on a user input.
+print("\nPython has inputted your personal information.")
+print(f"\nHi, my name is {userName}. I am{userAge: .0f} years old and I live in {userAddress}.")
